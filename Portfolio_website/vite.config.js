9ec419d160640eb5a5b690f849c86d0368fcebc3
Ajay-soni18 <ajaysoni7296@gmail.com>
2025-04-02 10:58:47 +0530

    ##

--- Portfolio_website/vite.config.js
+++ Portfolio_website/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/PORTFOLIO_REACT/",
+  base: "/Portfolio_react/",
   plugins: [react()],
 });
